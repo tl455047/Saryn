39f1b85839c292a3b59a61b31ad4f27ce6b155a5
tl455047 <tl445047925@gmail.com>
2022-03-17 18:17:17 +0800

    How to limit input size

--- src/afl-fuzz-one.c
+++ src/afl-fuzz-one.c
@@ -2820,7 +2820,7 @@ havoc_stage:
   }
   // we should not apply this analysis for every seed.
   if (afl->symbolic_mode && afl->ready_for_symbolic 
-        && (u32)len <= SYMBOLIC_MAX_FILE_SIZE) {
+        && (u32)len <= afl->cmplog_max_filesize) {
     
     invoke_symbolic(afl, out_buf, in_buf, len);
     
